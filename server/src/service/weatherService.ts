ec584e12eae1eafa905fa49d5de08d0adc722255
Spicy Moo Deng <188100478+jo-price@users.noreply.github.com>
2025-02-27 12:43:11 -0600

    add weatherService methods

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -8,16 +8,61 @@ dotenv.config();
 // TODO: Complete the WeatherService class
 class WeatherService {
   // TODO: Define the baseURL, API key, and city name properties
+
+
   // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {}
+  private async fetchLocationData(query: string) {
+    let locationData: Coordinates = {
+      lat:'',
+      lon:'',
+      }
+      const geoQuery = this.buildGeocodeQuery(query) 
+      
+      const geocode = await fetch(geoQuery);
+      
+      if (!geocode.ok) {
+      throw new Error(Error:${geocode.statusText})
+      }
+      
+      const geocodeParsed = await geocode.json()
+      
+      locationData = {
+      lat: geocodeParsed[0].lat,
+      lon: geocodeParsed[0].lon
+      }
+      
+      return locationData    
+  };
+
   // TODO: Create destructureLocationData method
-  // private destructureLocationData(locationData: Coordinates): Coordinates {}
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    const { lat, lon } = locationData
+    let coordinates = { 
+    lat,
+    lon
+    } 
+  return coordinates
+  }
+
   // TODO: Create buildGeocodeQuery method
-  // private buildGeocodeQuery(): string {}
+  private buildGeocodeQuery(): string {
+    return ${this.baseURL}/geo/1.0/direct?q=${query}&limit=1&appid=${this.apiKey}
+  }
+  
   // TODO: Create buildWeatherQuery method
-  // private buildWeatherQuery(coordinates: Coordinates): string {}
+  private buildWeatherQuery(coordinates: Coordinates): string {
+    return ${this.baseURL}/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${this.apiKey}&units=imperial
+  }
+
   // TODO: Create fetchAndDestructureLocationData method
-  // private async fetchAndDestructureLocationData() {}
+  private async fetchAndDestructureLocationData() {
+    const locationData = await
+    this.fetchLocationData(this.cityName) 
+    const coordinates = this.destructureLocationData(locationData)
+    return coordinates
+    
+    }
+  
   // TODO: Create fetchWeatherData method
   // private async fetchWeatherData(coordinates: Coordinates) {}
   // TODO: Build parseCurrentWeather method

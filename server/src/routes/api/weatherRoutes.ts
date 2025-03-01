e447fb912fa32120ff0360475b8d38a23025e374
Spicy Moo Deng <188100478+jo-price@users.noreply.github.com>
2025-03-01 11:36:20 -0600

    fix parameters

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -5,15 +5,15 @@ const router = Router();
 // import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', (_req, _res) => {
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req, res) => {});
+router.get('/history', async (_req, _res) => {});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req, res) => {});
+router.delete('/history/:id', async (_req, _res) => {});
 
 export default router;
